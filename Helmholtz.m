5f5d84306b1906e330dfe535d3842039eb4a4123
fredward094 <freddy.dominguez77@live.com>
2017-05-02 19:13:35 -0500

    Update Helmholtz.m

--- Helmholtz.m
+++ Helmholtz.m
@@ -10,14 +10,14 @@ x=linspace(ax,bx,n); y=linspace(ay,by,n);
 phiab=cos(pi*(y-ay)).*cosh(by-y);
 psiab=(y-ay).^2.*sin(pi*(y-ay)/(by-ay));
 u(:,1)=phiab; u(:,n)=psiab;
-
+h=lenght(x);
 
 F = cos(pi/2*(2*(x-ax)./(bx-ax)+1)).*sin(pi*(y-ay)./(by-ay));
 
 for j=2:n-1
     for i=2:n-1
         F(i,j) = cos(pi/2*(2*(x(i)-ax)./(bx-ax)+1)).*sin(pi*(y(j)-ay)./(by-ay));
-        u(i,j)= 1/(4-gamma)*(u(i-1,j)+u(i+1,j)+u(i,j-1)+u(i,j+1));
+        u(i,j)= 1/(4-gamma)*(u(i-1,j)+u(i+1,j)+u(i,j-1)+u(i,j+1))*(1/h);
     end
 end
 u(1,:)=u(2,2); u(n,:)=u(n-1,n-1);
